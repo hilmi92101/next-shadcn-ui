e138a6f1a6db2521ecf188679b74b0fe3a311cfd
hilmi <muhammadhilmi92101@gmail.com>
2024-02-07 03:16:53 +0800

    add skeleton example. change theme in globals css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,6 +24,9 @@ async function getRecipes(): Promise<Recipe[]> {
 
     const result = await fetch('http://localhost:4000/recipes')
 
+    // delay response
+    //await new Promise((resolve) => setTimeout(resolve, 3000))
+
     return result.json()
 }
 
@@ -37,6 +40,7 @@ export default async function Home() {
             <div className="grid grid-cols-3 gap-8">
 
                 {recipes.map(recipe => (
+
                     <Card key={recipe.id} className="flex flex-col justify-between">
                         <CardHeader className="flex-row gap-4 items-center">
                             <Avatar>
@@ -54,10 +58,11 @@ export default async function Home() {
                             <p>{recipe.description}</p>
                         </CardContent>
                         <CardFooter className="flex justify-between">
-                            <Button variant="destructive">View Recipe</Button>
+                            <Button>View Recipe</Button>
                             {recipe.vegan && <Badge variant="secondary">Vegan!</Badge>}
                         </CardFooter>
                     </Card>
+                    
                 ))}
 
             </div>
